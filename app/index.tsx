f2cbb4d5433bb5a55a7ad575e4c0166e4b0aacfc
Eduarda Calado <dudacal1600@hotmail.com>
2024-10-16 00:15:02 -0300

    Corrigindo tela home

---
+++ app/index.tsx
@@ -0,0 +1,15 @@
+import { Link } from 'expo-router';
+import { Text, View } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <View style={{
+        alignItems: "center",
+        justifyContent: "center",
+        flex: 1
+    }}>
+      <Text>Seja Bem-vindo!</Text>
+      <Link href={"/login"}>Login</Link>
+    </View>
+  );
+}
