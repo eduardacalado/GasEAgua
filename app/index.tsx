a2a95a05b9eac377e2be9b9aa31451b3cc45daf4
Eduarda Calado <dudacal1600@hotmail.com>
2024-10-22 16:01:37 -0300

    feat(ui): add background styles on the welcome-page

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,6 @@
+import { LinearGradient } from 'expo-linear-gradient';
 import { Link } from 'expo-router';
-import { Text, View } from 'react-native';
+import { Text, TouchableOpacity, View } from 'react-native';
 
 export default function HomeScreen() {
   return (
@@ -7,10 +8,40 @@ export default function HomeScreen() {
         alignItems: "center",
         justifyContent: "center",
         flex: 1,
-        backgroundColor: "red"
+        backgroundColor: "white"
     }}>
-      <Text>Seja Bem-vindo!</Text>
-      <Link href={"/login"}>Login</Link>
+      <LinearGradient
+      style={{
+        height: 370,
+        position: "absolute",
+        top: 0,
+        left: 0,
+        right: 0,
+        borderBottomLeftRadius: 92,
+        borderBottomRightRadius: 92
+      }}
+
+      colors={['#DB1A00', '#ED4200', '#FF6A00']}
+      start={{ x: 0, y: 1 }}
+      end={{ x: 1, y: 0 }}
+      />
+
+        <View style={{
+          backgroundColor: "white",
+          width: 325,
+          height: 200,
+          borderRadius: 20,
+          alignItems: "center",
+          justifyContent: "center",
+          elevation: 5
+        }}>
+          <Text>Seja Bem-vindo!</Text>
+          
+          <TouchableOpacity>
+            <Link href={"/login"}>Login</Link>
+          </TouchableOpacity>
+
+        </View>
     </View>
   );
 }
