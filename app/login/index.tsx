8a59af211b24d5d55921bca7d44bbe114589af6a
Eduarda Calado <dudacal1600@hotmail.com>
2024-11-06 09:38:19 -0300

    feat(ui): login screen inputs and button

--- app/login/index.tsx
+++ app/login/index.tsx
@@ -1,7 +1,36 @@
-import { SafeAreaView, Text, View } from "react-native";
+import { TouchableOpacity} from "react-native";
+import * as S from './styles'
 
 export default function Login() {
     return (
-        <SafeAreaView><View><Text>login</Text></View></SafeAreaView>
+        <S.SafeAreaViewBackground>
+      < S.LinearGradientContainer
+      colors={['#DB1A00', '#ED4200', '#FF6A00']}
+      start={{ x: 0, y: 1 }}
+      end={{ x: 1, y: 0 }}
+      />
+
+        <S.Container style={{elevation: 5}}>
+
+          <S.InputArea placeholder="Email"/>
+
+          <S.InputArea placeholder="Senha"/>
+                        
+          <TouchableOpacity>
+            <S.LoginButton
+              colors={['#DB1A00', '#ED4200', '#FF6A00']}
+              start={{ x: 0, y: 1 }}
+              end={{ x: 1, y: 0 }}
+            >
+
+              <S.LoginButtonText>
+                Entrar
+              </S.LoginButtonText>
+            </S.LoginButton>
+          </TouchableOpacity>
+
+        </ S.Container>
+
+    </S.SafeAreaViewBackground>
     )
 }    
---
+++ app/login/styles.ts
@@ -0,0 +1,49 @@
+import styled from "styled-components/native";
+import { LinearGradient } from 'expo-linear-gradient';
+
+export const SafeAreaViewBackground = styled.SafeAreaView`
+    align-items: center;
+    justify-content: center;
+    flex: 1;
+    background-color: white;
+`;
+
+export const LinearGradientContainer = styled(LinearGradient)`
+    height: 370px;
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    border-bottom-left-radius: 92px;
+    border-bottom-right-radius: 92px;
+`;
+
+export const Container = styled.View`
+    background-color: white;
+    width: 325px;
+    height: 200px;
+    border-radius: 20px;
+    align-items: center;
+    justify-content: center;
+    gap: 15px;
+`;
+
+export const InputArea = styled.TextInput`
+    padding: 10px;
+    width: 275px;
+    align-items: center;
+    border-radius: 25px;
+    background-color: #D9D9D9;
+`;
+
+export const LoginButton = styled(LinearGradient)`
+    padding: 10px;
+    width: 125px;
+    align-items: center;
+    border-radius: 25px;
+`;
+
+export const LoginButtonText = styled.Text`
+    color: white;
+    font-weight: bold;
+`;
