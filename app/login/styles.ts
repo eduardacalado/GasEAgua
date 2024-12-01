308499e866fa01c9b810132f293f5166069b84c5
Maria Eduarda <161755667+eduardacalado@users.noreply.github.com>
2024-12-01 12:31:24 -0300

    refactor: implement theme in styles (#7)

--- app/login/styles.ts
+++ app/login/styles.ts
@@ -1,48 +1,48 @@
 import styled from "styled-components/native";
 import { LinearGradient } from 'expo-linear-gradient';
+import theme from "../../styles/theme"
 
 export const SafeAreaViewContainer = styled.SafeAreaView`
     align-items: center;
     justify-content: center;
     flex: 1;
-    background-color: white;
+    background-color: ${theme.colors.background.white};
 `;
 
 export const Container = styled.View`
-    background-color: white;
+    background-color: ${theme.colors.background.white};
     width: 325px;
-    height: 200px;
-    border-radius: 20px;
+    height: 210px;
+    border-radius: ${theme.size.m6};
     align-items: center;
     justify-content: center;
-    gap: 15px;
+    gap: ${theme.size.m4};
     elevation: 5;
 `;
 
 export const InputArea = styled.View`
-    padding: 10px;
-    width: 275px;
+    padding: ${theme.size.m2};
+    width: ${theme.size.m15};
     align-items: center;
-    border-radius: 25px;
-    background-color: #D9D9D9;
+    border-radius: ${theme.size.m7};
+    background-color: ${theme.colors.input.background};
     flex-direction: row;
 `;
 
 export const Input = styled.TextInput`
   flex: 1;
-  font-size: 16px;
-  color: #000000;
-  margin-left: 10px;
+  font-size: ${theme.size.m4};
+  margin-left: ${theme.size.m2};
 `;
 
 export const LoginButton = styled(LinearGradient)`
-    padding: 10px;
+    padding: ${theme.size.m2};
     width: 125px;
     align-items: center;
-    border-radius: 25px;
+    border-radius: ${theme.size.m7};
 `;
 
 export const LoginButtonText = styled.Text`
-    color: white;
-    font-weight: bold;
+    color: ${theme.colors.button.primary.text};
+    font-weight: ${theme.font.weight.bold};
 `;
